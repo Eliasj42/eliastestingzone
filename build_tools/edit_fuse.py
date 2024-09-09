cab30ee30164ca6d843f4180d53428193e271896
Elias Joseph <eljoseph@amd.com>
2024-09-09 03:51:27 +0000

    test6

--- build_tools/edit_fuse.py
+++ build_tools/edit_fuse.py
@@ -3,11 +3,11 @@ import re
 
 key = str(sys.argv[1])
 
-f_ = open("fuse_connection.yaml")
+f_ = open("build_tools/fuse_connection.yaml")
 s = f_.read()
 f_.close()
 
 s = re.sub("replace_string", key, s)
-f_ = open("fuse_connection2.yaml", "w+")
+f_ = open("build_tools/fuse_connection2.yaml", "w+")
 f_.write(s)
 f_.close()
